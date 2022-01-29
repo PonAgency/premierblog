8b38c3357113e12bf5caf9d0e2011e48fc2598e9
PonAgency <contact@pon-agency.com>
2022-01-29 21:24:18 +0100

    2021.01.29 Authentification et utilisateur

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/create'
+  get 'users/uodate'
+  get 'users/destroy'
+  devise_for :users
   resources :posts
   root to: 'home#index'
 end
