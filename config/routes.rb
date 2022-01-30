28b24db39cc0271c803b90a9b584dc5019f4e15c
PonAgency <contact@pon-agency.com>
2022-01-30 20:28:38 +0100

    Ajout de devise et correction du bug déconnexion

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/create'
-  get 'users/uodate'
-  get 'users/destroy'
   devise_for :users
-  resources :posts
   root to: 'home#index'
+  resources :users
+  resources :posts
 end
