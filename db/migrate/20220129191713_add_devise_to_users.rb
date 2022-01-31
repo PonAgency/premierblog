5382f4d48004f9f1afd12ef08b88593f895b3dbb
PonAgency <contact@pon-agency.com>
2022-01-31 23:47:08 +0100

    reset change migration

--- db/migrate/20220129191713_add_devise_to_users.rb
+++ db/migrate/20220129191713_add_devise_to_users.rb
@@ -4,22 +4,17 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, defaut: ""
-      t.string :encrypted_password, null: false, defaut: ""
 
       ## Recoverable
-      t.string :reset_password_token
-      t.datetile :reset_password_sent_at
 
       ## Rememberable
-      t.datetime :remember_created_at
 
       ## Trackable
-      t.integer  :sign_in_count, default: 0, null: false
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.string   :current_sign_in_ip
-      t.string   :last_sign_in_ip
+      # t.integer  :sign_in_count, default: 0, null: false
+      # t.datetime :current_sign_in_at
+      # t.datetime :last_sign_in_at
+      # t.string   :current_sign_in_ip
+      # t.string   :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
@@ -32,8 +27,6 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.timestamps null: false
-
 
       # Uncomment below if timestamps were not included in your original model.
       # t.timestamps null: false
@@ -44,9 +37,6 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
   end
 
   def self.down
-
-    add_index :users, :email,             unique: true
-    add_index :users :reset_password_token, unique: true
     # By default, we don't want to make any assumption about how to roll back a migration when your
     # model already existed. Please edit below which fields you would like to remove in this migration.
     raise ActiveRecord::IrreversibleMigration
